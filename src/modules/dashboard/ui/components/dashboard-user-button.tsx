ca1bc05add634880484889e0b82bbf7fd79ec11d
MuhammadUmar7195 <mmuhammadumar869@gmail.com>
2025-08-03 22:55:53 +0500

    create a mobile responsive menu trigger for check user details

--- src/modules/dashboard/ui/components/dashboard-user-button.tsx
+++ src/modules/dashboard/ui/components/dashboard-user-button.tsx
@@ -9,15 +9,29 @@ import {
     DropdownMenuSeparator,
     DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import {
+    Drawer,
+    DrawerClose,
+    DrawerContent,
+    DrawerDescription,
+    DrawerFooter,
+    DrawerHeader,
+    DrawerTitle,
+    DrawerTrigger,
+} from "@/components/ui/drawer";
 import { Avatar, AvatarImage } from "@/components/ui/avatar";
 import { GenerateAvatar } from "@/components/generateAvatar";
 import { IoIosArrowDown } from "react-icons/io";
 import { PiCreditCardFill } from "react-icons/pi";
 import { TbLogout } from "react-icons/tb";
 import { useRouter } from "next/navigation";
+import { useIsMobile } from "@/hooks/use-mobile";
+import { Button } from "@/components/ui/button";
+
 
 const DashboardUserButton = () => {
     const router = useRouter();
+    const isMobile = useIsMobile();
     const { data, isPending } = authClient.useSession();
 
     const onLogout = async () => {
@@ -32,6 +46,55 @@ const DashboardUserButton = () => {
 
     if (isPending && !data?.user) {
         return null;
+    }
+
+    if (isMobile) {
+        return <>
+            <Drawer>
+                <DrawerTrigger className="flex rounded-lg border border-border/10 p-3 w-full flex-items-center justify-between bg-white/5 hover:bg-white/10 overflow-hidden gap-x-2">
+                    {data?.user?.image ? (
+                        <Avatar className="">
+                            <AvatarImage
+                                src={data.user.image}
+                                alt="User Avatar"
+                                className="rounded-full w-8 h-8 object-cover object-left"
+                            />
+                        </Avatar>
+                    ) : (
+                        <GenerateAvatar
+                            seed={data?.user?.name}
+                            variant="initials"
+                            className="size-9 mr-3"
+                        />
+                    )}
+                    <div className="flex flex-col justify-center min-w-0 flex-1 overflow-hidden text-left">
+                        <p className="text-sm font-medium truncate leading-tight">{data?.user?.name}</p>
+                        <p className="text-xs text-muted-foreground truncate">{data?.user?.email}</p>
+                    </div>
+                    <IoIosArrowDown className="size-4 shrink-0 cursor-pointer" />
+                </DrawerTrigger>
+                <DrawerContent>
+                    <DrawerHeader>
+                        <DrawerTitle>
+                            {data?.user?.name}
+                        </DrawerTitle>
+                        <DrawerTitle>
+                            {data?.user?.email}
+                        </DrawerTitle>
+                    </DrawerHeader>
+                    <DrawerFooter>
+                        <Button variant="outline" onClick={() => { }}>
+                            <PiCreditCardFill className="size-4 ml-2" />
+                            Billing
+                        </Button>
+                        <Button variant="outline" onClick={onLogout}>
+                            <TbLogout className="size-4 ml-2" />
+                            Logout
+                        </Button>
+                    </DrawerFooter>
+                </DrawerContent>
+            </Drawer>
+        </>
     }
 
     return (
