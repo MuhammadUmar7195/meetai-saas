19509fe1fbaaff74dc5ad9912ef37c8cb4625a51
MuhammadUmar7195 <mmuhammadumar869@gmail.com>
2025-08-16 12:30:39 +0500

    Complete a agent table and empty state svg

--- src/modules/agents/server/procedures.ts
+++ src/modules/agents/server/procedures.ts
@@ -3,16 +3,22 @@ import { db } from "@/db";
 import { agents } from "@/db/schema";
 import {
   createTRPCRouter,
-  baseProcedure,
   protectedProcedure,
 } from "@/trpc/init";
 import { agentSchema } from "../schema";
-import { eq } from "drizzle-orm";
+import { eq, getTableColumns, sql } from "drizzle-orm";
 
 export const agentsRouter = createTRPCRouter({
   //TODO: Change `getOne` to use `protectedProcedure`
   getOne: protectedProcedure.input(z.object({ id: z.string() })).query(async ({ input }) => {
-    const [existingAgent] = await db.select().from(agents).where(eq(agents.id, input.id));
+    const [existingAgent] = await db
+    .select({
+      ...getTableColumns(agents),
+      meetingCount: sql<number>`5`
+    })
+    .from(agents)
+    .where(eq(agents.id, input.id));
+
     return existingAgent;
   }),
   //TODO: Change `getMany` to use `protectedProcedure`
